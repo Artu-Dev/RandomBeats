3993ddd03735892bd12539f8ad43eb51b8549124
Artu Reis <120587481+Artu-Dev@users.noreply.github.com>
2023-10-20 13:00:18 -0300

    fix rangeInput starting on 0

--- src/components/RangeInput/RangeInput.tsx
+++ src/components/RangeInput/RangeInput.tsx
@@ -28,7 +28,7 @@ export const RangeInput: FunctionComponent<IRange> = ({text, id, defaultValue, c
 					defaultValue={defaultValue}
 					disabled={disabled}
 				/>
-				<output>{rangeRef.current?.value || "0"}</output>
+				<output>{rangeRef.current?.value || `${defaultValue}`}</output>
 			</span>
 		</span>
 	)
